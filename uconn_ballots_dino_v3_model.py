ac2c7ebab3c4fdae823cd04d241baf2c0e4223b9
Ethen Puls <epuls56@gmail.com>
2026-04-25 12:47:09 -0400

    model changed to match cub dino

--- uconn_ballots_dino_v3_model.py
+++ uconn_ballots_dino_v3_model.py
@@ -2,9 +2,14 @@ import torch
 import torch.nn.functional as F
 
 class DINOv3UConnModel(torch.nn.Module):
-    def __init__(self, num_classes=2, repo_dir="D:/dinov3/dinov3",
-                 weights_path="../dinov3_vits16_pretrain_lvd1689m.pth",
-                 freeze_backbone=True):
+    def __init__(
+            self, 
+            num_classes=2, 
+            repo_dir="D:/dinov3/dinov3",
+            weights_path="../dinov3_vits16_pretrain_lvd1689m.pth",
+            freeze_backbone=True):
+        super().__init__()
+
         super().__init__()
 
         self.backbone = torch.hub.load(
@@ -14,11 +19,10 @@ class DINOv3UConnModel(torch.nn.Module):
             weights=weights_path,
         )
 
-        self.freeze_backbone = freeze_backbone
         if freeze_backbone:
             for p in self.backbone.parameters():
                 p.requires_grad = False
-            self.backbone.eval()
+
 
         self.classifier = torch.nn.Sequential(
             torch.nn.LayerNorm(384),
@@ -28,11 +32,7 @@ class DINOv3UConnModel(torch.nn.Module):
             torch.nn.Linear(512, num_classes),
         )
 
-    def forward(self, x):
-        if self.freeze_backbone:
-            with torch.no_grad():
-                features = self.backbone(x)
-        else:
-            features = self.backbone(x)
-
-        return self.classifier(features.float())
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        outputs = self.backbone(x)
+        logits = self.classifier(outputs)
+        return logits
